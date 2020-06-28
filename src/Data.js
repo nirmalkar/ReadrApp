f79a2b1c11c152fe95a0501acbe99d9099d17257
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-28 13:57:50 +0530

    DATA: hard coded data for showing book cards

---
+++ src/Data.js
@@ -0,0 +1,38 @@
+export const bookData = [
+  {
+    id: 1,
+    title: "The Alchemist",
+    image: "../../assets/images/the-alchemint.jpeg",
+    description:
+      "This book is all about tour and travel. This book gives idea about what one should do and what one should not while travelling to the world",
+    stars: 5,
+    link: "./assets/pdfs/The-Alchemist.pdf",
+  },
+  {
+    id: 2,
+    title: "The Alchemist",
+    image: "../../assets/images/the-alchemint.jpeg",
+    description:
+      "This book is all about tour and travel. This book gives idea about what one should do and what one should not while travelling to the world",
+    stars: 5,
+    link: "./assets/pdfs/The-Alchemist.pdf",
+  },
+  {
+    id: 3,
+    title: "The Alchemist",
+    image: "../../assets/images/the-alchemint.jpeg",
+    description:
+      "This book is all about tour and travel. This book gives idea about what one should do and what one should not while travelling to the world",
+    stars: 5,
+    link: "./assets/pdfs/The-Alchemist.pdf",
+  },
+  {
+    id: 3,
+    title: "The Alchemist",
+    image: "../../assets/images/the-alchemint.jpeg",
+    description:
+      "This book is all about tour and travel. This book gives idea about what one should do and what one should not while travelling to the world",
+    stars: 5,
+    link: "./assets/pdfs/The-Alchemist.pdf",
+  },
+];
