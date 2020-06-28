482c2c8fa61211ff5b8c3a03efba69327bdb3ff0
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-28 13:59:35 +0530

    BOOK_CARD: mapped books from bookData to as book cards

--- src/components/Landing/BookCard.js
+++ src/components/Landing/BookCard.js
@@ -1,26 +1,31 @@
 import React from "react";
 
+import { bookData } from "../../Data";
+
 const BookCard = (props) => {
+  console.log(bookData);
   return (
-    <div>
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img
-              src="https://bulma.io/images/placeholders/1280x960.png"
-              alt="Placeholder image"
-            />
-          </figure>
-        </div>
-        <div className="card-content">
-          <p className="title ">The Book</p>
-          <p className="subtitle ">
-            This book is all about tour and travel. This book gives idea about
-            what one should do and what one should not while travelling to the
-            world
-          </p>
-        </div>
-      </div>
+    <div className="columns">
+      {bookData.map((book) => {
+        return (
+          <div key={book.id} className="column is-3 mt-6">
+            <div className="card">
+              <div className="card-image">
+                <figure className="image 5by4">
+                  <img
+                    src="../../assets/images/alchemist.jpeg"
+                    alt={book.title}
+                  />
+                </figure>
+              </div>
+              <div className="card-content">
+                <p className="title ">{book.title}</p>
+                <p className="subtitle">{book.description}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
