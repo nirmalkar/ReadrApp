993e097d8aee16812d8ca316f35988fc645a7cfd
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-30 02:52:44 +0530

    INDEX: provider imported from react redux and connected store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+
 import App from "./App";
+import store from "./appRedux/store";
 
 import "./scss/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <App />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <App />
+        </Switch>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
