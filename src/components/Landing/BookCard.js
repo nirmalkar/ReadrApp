23310a9b922b2c662982d6c296db069ee365144c
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-29 20:37:27 +0530

    BOOK_CARD: checked proptypes and linked each book card to the respective books

--- src/components/Landing/BookCard.js
+++ src/components/Landing/BookCard.js
@@ -1,29 +1,46 @@
 import React from "react";
-
-import { bookData } from "../../Data";
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const BookCard = (props) => {
   return (
-    <div className="columns">
-      {bookData.map((book) => {
-        return (
-          <div key={book.id} className="column is-3 mt-6">
-            <div className="card">
-              <div className="card-image">
-                <figure className="image 5by4">
-                  <img src={book.image} alt={book.title} />
-                </figure>
-              </div>
-              <div className="card-content">
-                <p className="title ">{book.title}</p>
-                <p className="subtitle">{book.description}</p>
-              </div>
+    <div className="column is-3">
+      {props.book ? (
+        <div className="card mt-6">
+          <Link to={`/book/${props.book.id}`}>
+            <div className="card-image">
+              <figure className="image 5by4">
+                <img src={props.book.image} alt={props.book.title} />
+              </figure>
             </div>
-          </div>
-        );
-      })}
+            <div className="card-content">
+              <p className="title ">{props.book.title}</p>
+              <p className="subtitle">{props.book.description}</p>
+            </div>
+          </Link>
+          <footer className="card-footer">
+            <a href="#" className="card-footer-item">
+              Edit
+            </a>
+            <a href="#" className="card-footer-item">
+              Delete
+            </a>
+          </footer>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
+};
+
+BookCard.propTypes = {
+  book: propTypes.shape({
+    image: propTypes.string.isRequired,
+    title: propTypes.string.isRequired,
+    description: propTypes.string.isRequired,
+    id: propTypes.number.isRequired,
+  }),
 };
 
 export default BookCard;
