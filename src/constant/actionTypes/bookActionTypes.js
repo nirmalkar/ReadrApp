fffa6f6a246e1d1bbe0c486ffa070c46c7cc433d
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-30 03:01:40 +0530

    ACTION_TYPES: created for add, update, and delete book

---
+++ src/constant/actionTypes/bookActionTypes.js
@@ -0,0 +1,8 @@
+// Action type for add a book
+export const SAVE_BOOK = "SAVE_BOOK";
+
+// Action type for delete a book
+export const DELETE_BOOK = "DELETE_BOOK";
+
+// Action type for update a book
+export const UPDATE_BOOK = "UPDATE_BOOK";
