f1409c6beb0e6a92303e17d00b6edb91a68ae0b0
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-30 02:57:14 +0530

    ROUTE: created for add book and view book by id

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Landing from "../pages/Landing";
 import AddBook from "../pages/AddBook";
-import Admin from "../pages/Admin";
+import Book from "../pages/Book";
+import BookList from "../pages/BookList";
 
 const Routes = () => {
   return (
     <>
-      <Route exact path="/addbook" component={AddBook} />
-      <Route exact path="/admin" component={Admin} />
+      <Route path="/addbook" component={AddBook} />
+      <Route exact path="/books" component={BookList} />
+      <Route exact path="/book/:id" component={Book} />
       <Route exact path="/" component={Landing} />
     </>
   );
