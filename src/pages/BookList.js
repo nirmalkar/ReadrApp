a202731d9b155a4c6abeab81b99f303604717fb9
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-06 03:25:33 +0530

    COMPONENT-BOOKLIST: book filter functionality added

--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+
 import BookCard from "../components/Landing/BookCard";
 import SearchBar from "../components/searchBar";
 
 const BookList = (props) => {
   const bookList = useSelector((state) => state.bookList);
+  const { bookSearch } = useSelector((state) => state.bookSearch);
+
   const { books } = bookList;
   return (
     <section className="section">
@@ -14,12 +17,15 @@ const BookList = (props) => {
             <SearchBar />
           </div>
         </div>
-
         <h1 className="title has-text-centered">Books</h1>
         <div className="columns is-multiline">
-          {books.map((book, i) => {
-            return <BookCard book={book} key={i} />;
-          })}
+          {books
+            .filter((book) =>
+              book.title.toLowerCase().includes(bookSearch.toLowerCase())
+            )
+            .map((book, i) => {
+              return <BookCard book={book} key={i} />;
+            })}
         </div>
       </div>
     </section>
