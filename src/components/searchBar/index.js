8cadfc8ef410bedacb6c43bb3b287ba486266b18
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-06 03:24:17 +0530

    COMPONENT-SEARCH_BAR: dispatched search book action accessed store data using useSelector

--- src/components/searchBar/index.js
+++ src/components/searchBar/index.js
@@ -1,19 +1,39 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { searchBook } from "../../appRedux/action/searchAction";
+import { useInputState } from "../../hooks/useInputState";
 
 const SearchBar = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { bookSearch } = useSelector((state) => state.bookSearch);
+  const [search, setSearch] = useInputState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(searchBook(search));
+    history.push("/books");
+  };
   return (
-    <div className="field has-addons">
-      <p className="control is-expanded">
-        <input
-          className="input"
-          type="text"
-          placeholder="Find something to read"
-        />
-      </p>
-      <p className="control">
-        <button className="button is-black">Search</button>
-      </p>
-    </div>
+    <form onSubmit={submitHandler}>
+      <div className="field has-addons">
+        <p className="control is-expanded">
+          <input
+            defaultValue={bookSearch}
+            className="input"
+            type="text"
+            placeholder="Find something to read"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </p>
+        <p className="control">
+          <button type="submit" className="button is-black">
+            Search
+          </button>
+        </p>
+      </div>
+    </form>
   );
 };
 
