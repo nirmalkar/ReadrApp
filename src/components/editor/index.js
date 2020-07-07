359a41878d4675bdccf0055c609104848c4ec2b9
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-07 19:10:01 +0530

    COMPONENT-EDITOR:proptypes defined for bookcontent

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -4,11 +4,12 @@ import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const Editor = (props) => {
-  console.log(props);
   return (
     <div>
       <CKEditor
+        data={props.bookContent}
         editor={ClassicEditor}
+        style={{ minHeight: "500px" }}
         onChange={(event, editor) => {
           const data = editor.getData();
           props.setBookContent(data);
@@ -19,5 +20,6 @@ const Editor = (props) => {
 };
 Editor.propTypes = {
   setBookContent: propTypes.func.isRequired,
+  bookContent: propTypes.string.isRequired,
 };
 export default Editor;
